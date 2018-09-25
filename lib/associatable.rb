3c8f46284b708399faf14d652d4bf7f72904e03c
Jeff <jeffreyfang123@gmail.com>
2018-09-26 01:27:12 +0900

    Factor with assoc_options

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -49,27 +49,32 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  def belongs_to(name, options = {}) 
-    options = BelongsToOptions.new(name, options)
-    define_method(name) do
+  def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    define_method(name) do 
+      options = self.class.assoc_options[name]
       key_val = self.send(options.foreign_key)
-      options
-      .model_class
-      .where(:id => key_val)
-      .first
+      options 
+        .model_class 
+        .where(:id => key_val)
+        .first
     end 
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self, options)
-    define_method(name) do
+    self.assoc_options[name] = HasManyOptions.new(name, self, options)
+    define_method(name) do 
+      options = self.class.assoc_options[name]
       key_val = self.id
       options
-      .model_class
-      .where(options.foreign_key => key_val)
+        .model_class
+        .where(options.foreign_key => key_val)
     end 
   end
 
+  def assoc_options
+    @assoc_options ||= {}
+  end
 end
 
 
